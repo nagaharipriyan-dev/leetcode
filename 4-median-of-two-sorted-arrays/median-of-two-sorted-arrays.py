c97e7691074b8fb34b3b79286bd80a58fc2b2e52
nagaharipriyan-dev <nagaharipriyan.dev@gmail.com>
2026-03-03 10:44:15 +0530

    Time: 4 ms (29.94%) | Memory: 19.8 MB (5.09%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -3,9 +3,12 @@ class Solution:
         merged_array = list(heapq.merge(nums1, nums2))
         arr_len = len(merged_array)
 
-        mid_ind = int(arr_len / 2)
+        median = None
 
         if arr_len % 2 == 0:
-            return (merged_array[mid_ind] + merged_array[mid_ind - 1]) / 2
+            mid_ind = int(arr_len / 2)
+            median = (merged_array[mid_ind] + merged_array[mid_ind - 1]) / 2
         else:
-            return merged_array[mid_ind]
+            mid_ind = int(arr_len // 2)
+            median = merged_array[mid_ind]
+        return median
